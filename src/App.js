424ca285449c663b6f8ce55da028a7b7160e1bea
derkrisz <krisztian.der@gmail.com>
2018-02-19 20:32:39 +0000

    empty working moviebox component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import MovieBox from './containers/MovieBox'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <MovieBox />
     );
   }
 }
---
+++ src/containers/MovieBox.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class MovieBox extends Component {
+  render() {
+    return <div className="movie-box">I am a movie box</div>;
+  }
+}
+
+export default MovieBox;
