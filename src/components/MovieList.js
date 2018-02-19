d204c6f5265b1c9aac63c39d716adf120a454f46
derkrisz <krisztian.der@gmail.com>
2018-02-19 20:43:09 +0000

    empty working movielist component/hierarchy

---
+++ src/components/MovieList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div className="movie-list">I am a movie list</div>
+    );
+  }
+}
+
+export default MovieList;
--- src/containers/MovieBox.js
+++ src/containers/MovieBox.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import MovieList from '../components/MovieList'
 
 class MovieBox extends Component {
   render() {
-    return <div className="movie-box">I am a movie box</div>;
+    return(
+      <div className="movie-box">
+        <MovieList />
+      </div>
+    )
   }
 }
 
